91e90c06762024f0a704bec9aee1cfea3c6c8256
sebestindragos <sebestin.dragos@gmail.com>
2025-10-26 19:24:09 +0100

    feat: add ui library

---
+++ libraries/ui/src/index.ts
@@ -0,0 +1,22 @@
+// Re-export all UI components
+export * from './components/ui/button';
+export * from './components/ui/input';
+export * from './components/ui/label';
+export * from './components/ui/checkbox';
+export * from './components/ui/switch';
+export * from './components/ui/dialog';
+export * from './components/ui/toast';
+export * from './components/ui/toaster';
+export * from './components/ui/card';
+export * from './components/ui/badge';
+export * from './components/ui/alert';
+export * from './components/ui/select';
+export * from './components/ui/radio-group';
+export * from './components/ui/tabs';
+export * from './components/ui/accordion';
+
+// Re-export hooks
+export * from './hooks/use-toast';
+
+// Re-export utilities
+export * from './lib/utils';
