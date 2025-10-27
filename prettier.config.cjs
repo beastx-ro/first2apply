2665eb8a0c2f95dde7b865acd630095a42980378
sebestindragos <sebestin.dragos@gmail.com>
2025-10-27 20:12:53 +0100

    ci: prettier config

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -2,13 +2,26 @@ module.exports = {
   printWidth: 120,
   singleQuote: true,
   importOrder: [
-    'env',
-    '^[./].*instrumentation$', // More specific pattern for instrumentation imports
-    '<THIRD_PARTY_MODULES>',
-    '^[./]',
+    "env",
+    "react",
+    "^[./].*instrumentation$", // More specific pattern for instrumentation imports
+    "<THIRD_PARTY_MODULES>",
+    "^[./]",
   ],
   importOrderSeparation: true,
   importOrderSortSpecifiers: true,
-  importOrderParserPlugins: ['explicitResourceManagement', 'typescript', 'jsx'],
-  plugins: ['@trivago/prettier-plugin-sort-imports', 'prettier-plugin-tailwindcss'],
+  importOrderParserPlugins: ["explicitResourceManagement", "typescript", "jsx"],
+  plugins: ["@trivago/prettier-plugin-sort-imports", "prettier-plugin-tailwindcss"],
+  overrides: [
+    {
+      files: ["**/libraries/ui/src/**/*.{ts,tsx,css}"],
+      options: {
+        printWidth: 80,
+        singleQuote: false,
+        trailingComma: "es5",
+        semi: false,
+        plugins: ["prettier-plugin-tailwindcss"],
+      },
+    },
+  ],
 };
