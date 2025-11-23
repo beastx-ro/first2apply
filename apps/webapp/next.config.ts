f0a21a8999a9333f2b5ab451aff82d78135c9e09
sebestindragos <sebestin.dragos@gmail.com>
2025-11-23 19:15:04 +0100

    fix: next config

--- apps/webapp/next.config.ts
+++ apps/webapp/next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  output: 'standalone', // Required for Azure Static Web Apps
 };
 
 export default nextConfig;
