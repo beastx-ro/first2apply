0b3ad0ff995a4209729a6b7e0fe444fd711160f0
sebestindragos <sebestin.dragos@gmail.com>
2025-11-23 17:22:08 +0100

    ci: try terraform deploy to azure static website

--- apps/webapp/next.config.ts
+++ apps/webapp/next.config.ts
@@ -1,8 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  output: 'standalone', // Required for Azure Static Web Apps
 };
 
 export default nextConfig;
