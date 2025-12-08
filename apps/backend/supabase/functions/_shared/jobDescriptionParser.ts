282efc59784a885cbf3ec077408e19794fc9545e
sebestindragos <sebestin.dragos@gmail.com>
2025-12-08 22:23:41 +0100

    fix: update Dice job description selector

--- apps/backend/supabase/functions/_shared/jobDescriptionParser.ts
+++ apps/backend/supabase/functions/_shared/jobDescriptionParser.ts
@@ -38,7 +38,7 @@ const SITE_PROVIDER_QUERY_SELECTORS: Record<SiteProvider, SiteProviderQuerySelec
     description: ['#job-description'], // paywalled
   },
   [SiteProvider.dice]: {
-    description: [`h3 + div`],
+    description: [`#jobDescription`, '.job-description'],
   },
   [SiteProvider.bestjobs]: {
     description: ['div.relative.bg-surface div.p-4 div.mt-8.pt-8.border-t.border-input.prose'],
