a35ab4d5e4a484d99bb6559cb66fcda10f70d60c
sebestindragos <sebestin.dragos@gmail.com>
2026-03-13 18:50:44 +0100

    feat(config): remove standalone output option for next.js configuration

--- apps/webapp/next.config.ts
+++ apps/webapp/next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  output: 'standalone',
   allowedDevOrigins: ['app.local.first2apply.com', 'dragos.beastx.ro'],
 };
 
