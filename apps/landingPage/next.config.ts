9099144d772a360542f1f7b2a621f8ae5a19a3af
sebestindragos <sebestin.dragos@gmail.com>
2026-03-15 19:30:11 +0100

    fix(landingPage): nextjs build

--- apps/landingPage/next.config.ts
+++ apps/landingPage/next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  transpilePackages: ['@first2apply/ui'],
+  experimental: {
+    esmExternals: 'loose',
+  },
 };
 
 export default nextConfig;
